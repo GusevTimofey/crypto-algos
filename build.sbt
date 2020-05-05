af05243fbfa971bfc7d92f3bcc2a216287eb58ef
timofeygusev <gusev.t.s1@gmail.com>
2020-05-05 17:57:40 +0300

    more improvements

--- build.sbt
+++ build.sbt
@@ -8,7 +8,8 @@ libraryDependencies ++= Seq(
   "com.google.guava" % "guava"       % "28.2-jre",
   "org.typelevel"    %% "mouse"      % "0.24",
   "org.typelevel"    %% "simulacrum" % "1.0.0",
-  "org.typelevel"    %% "cats-core"  % "2.1.1"
+  "org.typelevel"    %% "cats-core"  % "2.1.1",
+  "io.estatico"      %% "newtype"    % "0.4.3"
 )
 
 scalacOptions in ThisBuild ++= Seq(
--- src/main/scala/algos/hash/SHA1.scala
+++ src/main/scala/algos/hash/SHA1.scala
@@ -9,40 +9,36 @@ import mouse.all._
 
 import scala.annotation._
 
-trait SHA1 {
-
-  def make(input: String): String
-
-}
+trait SHA1 { def make(input: String): String }
 
 object SHA1 {
-
   def apply: SHA1 = new SHA1 {
-
-    private val A: Long = 0x67452301L
-    private val B: Long = 0xEFCDAB89L
-    private val C: Long = 0x98BADCFEL
-    private val D: Long = 0x10325476L
-    private val E: Long = 0xC3D2E1F0L
-
     override def make(input: String): String = {
       val ar :: br :: cr :: dr :: er :: _ = formLastBlock(input).foldLeft(List(A, B, C, D, E)) {
-        case (a :: b :: c :: d :: e :: _, wInput: String) =>
-          val wMain: List[Long] = (16 to 79).foldLeft(wInput.grouped(32).toList.map(_.liftToLong)) {
-            case (acc: List[Long], i: Int) =>
-              acc appended leftRotate(acc(i - 3) ^ acc(i - 8) ^ acc(i - 14) ^ acc(i - 16), 1)
-          }
-
-          val (aN, bN, cN, dN, eN) = (0 to 79).foldLeft(a, b, c, d, e) {
-            case ((al, bl, cl, dl, el), i) =>
-              val newA = (leftRotate(al, 5) + Ft(bl, cl, dl, i) + el + Kt(i) + wMain(i)) & 0xFFFFFFFFL
-              (newA, al, leftRotate(bl, 30), cl, dl)
-          }
+        case (a :: b :: c :: d :: e :: _, wIn) =>
+          val w: List[Block64Bits] = wIn.value.grouped(32).toList.map(_.liftToLong |> Block64Bits.apply) |> computeWi
+          val (aN, bN, cN, dN, eN) = runBlockCycle(a, b, c, d, e, w)
           List(a + aN, b + bN, c + cN, d + dN, e + eN).map(_ & 0xFFFFFFFFL)
         case (l, _) => l
       }
       "%08x%08x%08x%08x%08x".format(ar, br, cr, dr, er)
     }
+
+    private def computeWi: List[Block64Bits] => List[Block64Bits] =
+      (wi: List[Block64Bits]) =>
+        (16 to 79).foldLeft(wi) {
+          case (acc, i) =>
+            val wi: Long = leftRotate(acc(i - 3).v ^ acc(i - 8).v ^ acc(i - 14).v ^ acc(i - 16).v, 1)
+            acc.appended(wi |> Block64Bits.apply)
+        }
+
+    private def runBlockCycle: (Long, Long, Long, Long, Long, List[Block64Bits]) => (Long, Long, Long, Long, Long) =
+      (a: Long, b: Long, c: Long, d: Long, e: Long, wRes: List[Block64Bits]) =>
+        (0 to 79).foldLeft(a, b, c, d, e) {
+          case ((al, bl, cl, dl, el), i) =>
+            val newA = (leftRotate(al, 5) + Ft(bl, cl, dl, i) + el + Kt(i) + wRes(i).v) & 0xFFFFFFFFL
+            (newA, al, leftRotate(bl, 30), cl, dl)
+        }
 
     private def Kt(round: Int): Long =
       if (round <= 19) 0x5A827999L
@@ -58,9 +54,9 @@ object SHA1 {
 
     private def leftRotate(block: Long, count: Int): Long = ((block << count) | (block >> (32 - count))) & 0xFFFFFFFFL
 
-    private def formLastBlock(input: String): List[String] = {
-      val inputBits: String              = input.map(_.asBits).foldLeft("")(_ + _)
-      val groupedInputBits: List[String] = inputBits.grouped(512).toList
+    private def formLastBlock(input: String): List[Block512Bits] = {
+      val inputBits: String                    = input.map(_.asBits).foldLeft("")(_ + _)
+      val groupedInputBits: List[Block512Bits] = inputBits.grouped(512).map(Block512Bits.apply).toList
       def expandLastBlock(tailBlockBits: String): String = {
         @tailrec def loop(acc: String): String =
           if ((acc.length + 64) % 512 == 0) {
@@ -71,8 +67,18 @@ object SHA1 {
           } else loop(acc + '0')
         loop(tailBlockBits + '1')
       }
-      val lastExpandedBlock: String = groupedInputBits.lastOption.getOrElse("") |> expandLastBlock
-      groupedInputBits dropRight 1 appendedAll lastExpandedBlock.grouped(512)
+      val lastExpandedBlock: String = groupedInputBits.lastOption.map(_.value).getOrElse("") |> expandLastBlock
+      groupedInputBits dropRight 1 appendedAll lastExpandedBlock.grouped(512).map(Block512Bits.apply)
     }
   }
+
+  private val A: Long = 0x67452301L
+  private val B: Long = 0xEFCDAB89L
+  private val C: Long = 0x98BADCFEL
+  private val D: Long = 0x10325476L
+  private val E: Long = 0xC3D2E1F0L
+
+  import io.estatico.newtype.macros.newtype
+  @newtype final case class Block512Bits(value: String)
+  @newtype final case class Block64Bits(v: Long)
 }
