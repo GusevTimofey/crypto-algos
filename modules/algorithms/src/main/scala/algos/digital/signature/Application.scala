589d9b33d71b935eb580762ce9067f0188e8f009
timofeygusev <gusev.t.s1@gmail.com>
2020-05-06 22:05:39 +0300

    digital signature done

--- modules/algorithms/src/main/scala/algos/digital/signature/Application.scala
+++ modules/algorithms/src/main/scala/algos/digital/signature/Application.scala
@@ -1,10 +1,36 @@
 package algos.digital.signature
 
-object Application extends App {
+import java.io.File
 
-  val digitalSignature = DS.apply
-  val pairs            = digitalSignature.formKeysPair(16)
-  val ciphered         = digitalSignature.cipher(BigInt(40000), pairs._2)
-  val deciphered       = digitalSignature.decipher(ciphered, pairs._1)
-  println(s"Algorithm finished. Result is: $deciphered")
+import algos.common.FileReader
+import algos.hash.HashFunction
+import cats.effect.{ ExitCode, IO, IOApp }
+import cats.syntax.functor._
+import algos.common.BitsLike.instances._
+import algos.common.FromBits.instances._
+import algos.common.BitsLike.ops._
+import algos.common.FromBits.ops._
+import algos.common.utils._
+import algos.common.types._
+
+object Application extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] =
+    (for {
+      _                <- IO(println(s"Enter source file path:"))
+      pathStr          <- IO(scala.io.StdIn.readLine())
+      path             <- IO(new File(pathStr).toPath)
+      reader           = FileReader[IO]
+      inputData        <- reader.readFrom(path)
+      _                <- IO(println(s"File data was read"))
+      sha1             = HashFunction.apply
+      hash             = sha1.make(inputData)
+      digitalSignature = DS.apply
+      hashBytes        = hash.flatMap(_.asBits.grouped(8).toList.map(_.liftToByte)).toArray
+      pairs            = digitalSignature.formKeysPair(1024)
+      ciphered         = digitalSignature.cipher(BigInt(hashBytes), pairs._2)
+      deciphered       = digitalSignature.decipher(ciphered, pairs._1)
+      _                <- IO(println(s"Algorithm finished. Result is: $deciphered"))
+      _                <- IO(println(s"Resulted hash is: $hash"))
+    } yield ()).as(ExitCode.Success)
 }
