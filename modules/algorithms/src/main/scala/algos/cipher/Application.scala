cb3e06e51a356495666e87c37bc5bbccf37eabf0
timofeygusev <gusev.t.s1@gmail.com>
2020-05-10 18:33:19 +0300

    cbc && ecb

--- modules/algorithms/src/main/scala/algos/cipher/Application.scala
+++ modules/algorithms/src/main/scala/algos/cipher/Application.scala
@@ -1,10 +1,31 @@
 package algos.cipher
 
+import algos.cipher.Cipher.BlowFishCipher
+
 object Application extends App {
 
-  val a  = Cipher.blowFish("abcdefc")
-  val in = Array.fill(8)(1: Byte)
-  val e  = a.encrypt(in)
-  val d  = a.decrypt(e)
-  println(d.sameElements(in))
+  def ecbR(): Unit = {
+    val blowfish: BlowFishCipher = Cipher.blowFish("blowfish")
+    val input: Array[Byte] = ("blowfish" * 8).getBytes()
+    println(s"input bytes ${input.toList}")
+    val encrypted: Array[Byte] = blowfish.ecbEncrypt(input)
+    val decrypted: Array[Byte] = blowfish.ecbDecrypt(encrypted)
+    println(s"decrypted bytes: ${decrypted.toList}")
+    println(decrypted.sameElements(input))
+  }
+  def cbcR: Unit = {
+    val blowfish: BlowFishCipher = Cipher.blowFish("blowfish")
+    val input: Array[Byte] = ("b1l1o1w1f1i1s1h1" * 100).getBytes()
+    val iv = "11111111".getBytes
+    println(s"input bytes ${input.length}")
+    val encrypted: Array[Byte] = blowfish.cbcEncrypt(input, iv)
+    println(s"encrypted bytes ${encrypted.length}")
+    val decrypted: Array[Byte] = blowfish.cbcDecrypt(encrypted, iv)
+    println(s"decrypted bytes: ${decrypted.length}")
+    println(decrypted.sameElements(input))
+    println(decrypted.toList)
+    println(input.toList)
+  }
+
+
 }
--- modules/algorithms/src/main/scala/algos/cipher/Cipher.scala
+++ modules/algorithms/src/main/scala/algos/cipher/Cipher.scala
@@ -4,18 +4,21 @@ import algos.common.utils._
 import com.google.common.primitives.Ints
 import mouse.all._
 
-trait Cipher {
-  def encrypt(input: Array[Byte]): Array[Byte]
-  def decrypt(output: Array[Byte]): Array[Byte]
+sealed trait Cipher {
+  def ecbEncrypt(input: Array[Byte]): Array[Byte]
+  def ecbDecrypt(input: Array[Byte]): Array[Byte]
+
+  def cbcEncrypt(input: Array[Byte], iv: Array[Byte]): Array[Byte]
+  def cbcDecrypt(input: Array[Byte], iv: Array[Byte]): Array[Byte]
+
+  def pcbcEncrypt(input: Array[Byte], iv: Array[Byte]): Array[Byte]
+  def pcbcDecrypt(input: Array[Byte], iv: Array[Byte]): Array[Byte]
 }
 
 object Cipher {
 
-  def blowFish(key: String): BlowFishCipher = {
-    val cipher = new BlowFishCipher
-    cipher.initKeys(key)
-    cipher
-  }
+  def blowFish(key: String): BlowFishCipher =
+    new BlowFishCipher().initKeys(key)
 
   final class BlowFishCipher extends Cipher {
 
@@ -25,19 +28,82 @@ object Cipher {
 
     val sMutable: Array[Array[Long]] = bfS
 
-    override def encrypt(input: Array[Byte]): Array[Byte] = {
+    override def ecbEncrypt(input: Array[Byte]): Array[Byte] = {
+      val blocks           = input.grouped(8).toArray
+      val (lastBlock, _)   = expandLastBlock(blocks.lastOption.getOrElse(Array.emptyByteArray), 0)
+      val resBlocks        = blocks.dropRight(1).appended(lastBlock)
+      val mutableEncrypted = new scala.collection.mutable.ArrayBuffer[Array[Byte]](blocks.length)
+      for (b <- resBlocks) mutableEncrypted.append(blockEncrypt(b))
+      mutableEncrypted.toArray.flatten
+    }
+
+    override def ecbDecrypt(input: Array[Byte]): Array[Byte] = {
+      val blocks           = input.grouped(8).toArray
+      val mutableEncrypted = new scala.collection.mutable.ArrayBuffer[Array[Byte]](blocks.length)
+      for (b <- blocks) mutableEncrypted.append(blockDecrypt(b))
+      mutableEncrypted.toArray.flatten
+    }
+
+    override def cbcEncrypt(input: Array[Byte], iv: Array[Byte]): Array[Byte] = {
+      val blocks                  = input.grouped(8).toArray
+      val (lastBlock, _)          = expandLastBlock(blocks.lastOption.getOrElse(Array.emptyByteArray), 0)
+      val resBlocks               = blocks.dropRight(1).appended(lastBlock)
+      val firstBlock: Array[Byte] = resBlocks.headOption.getOrElse(Array.emptyByteArray)
+      val xorIv: Array[Byte]      = firstBlock.zip(iv).map { case (b, b1) => (b ^ b1).toByte }
+      val mutableEncrypted        = new scala.collection.mutable.ArrayBuffer[Array[Byte]](blocks.length)
+      val encrypted               = blockEncrypt(xorIv)
+      mutableEncrypted.append(encrypted)
+      var lastBlockBytes = encrypted
+      for (b <- resBlocks.drop(1)) {
+        val bXor      = b.zip(lastBlockBytes).map { case (b, b1) => (b ^ b1).toByte }
+        val encrypted = blockEncrypt(bXor)
+        mutableEncrypted.append(encrypted)
+        lastBlockBytes = encrypted
+      }
+      val r = mutableEncrypted.toArray.flatten
+      r
+    }
+
+    override def cbcDecrypt(input: Array[Byte], iv: Array[Byte]): Array[Byte] = {
+      val blocks                  = input.grouped(8).toArray
+      val firstBlock: Array[Byte] = blocks.headOption.getOrElse(Array.emptyByteArray)
+      val mutableDecrypted        = new scala.collection.mutable.ArrayBuffer[Array[Byte]](blocks.length)
+      val encrypted               = blockDecrypt(firstBlock)
+      val fb                      = encrypted.zip(iv).map { case (b, b1) => (b ^ b1).toByte }
+      mutableDecrypted.append(fb)
+      var lastBlockBytes = firstBlock
+      for (b <- blocks.drop(1)) {
+        val encrypted = blockDecrypt(b)
+        val bXor      = encrypted.zip(lastBlockBytes).map { case (b, b1) => (b ^ b1).toByte }
+        mutableDecrypted.append(bXor)
+        lastBlockBytes = b
+      }
+      mutableDecrypted.toArray.flatten
+    }
+
+    def pcbcEncrypt(input: Array[Byte], iv: Array[Byte]): Array[Byte] = {
+
+      ???
+    }
+
+    def pcbcDecrypt(input: Array[Byte], iv: Array[Byte]): Array[Byte] = {
+
+      ???
+    }
+
+    def blockEncrypt(input: Array[Byte]): Array[Byte] = {
       val (l: Long, r: Long) =
         cipher(Ints.fromByteArray(input.take(4)), Ints.fromByteArray(input.takeRight(4)), pMutable, sMutable)
       Ints.toByteArray(l.toInt).appendedAll(Ints.toByteArray(r.toInt))
     }
 
-    override def decrypt(output: Array[Byte]): Array[Byte] = {
+    def blockDecrypt(output: Array[Byte]): Array[Byte] = {
       val (l: Long, r: Long) =
         decipher(Ints.fromByteArray(output.take(4)), Ints.fromByteArray(output.takeRight(4)), pMutable, sMutable)
       Ints.toByteArray(l.toInt).appendedAll(Ints.toByteArray(r.toInt))
     }
 
-    def initKeys(key: String): Unit = {
+    def initKeys(key: String): BlowFishCipher = {
       val keyBytes: Array[Byte] = key.getBytes
       if (keyBytes.length < 56 && keyBytes.length > 4) {
         val pKeys: Array[Long] =
@@ -72,6 +138,8 @@ object Cipher {
             sMutable(i)(k) = xl
             sMutable(i)(k + 1) = xr
           }
+
+        this
       } else throw new RuntimeException(s"Unsupported user key length! Got: ${keyBytes.length} key bytes.")
     }
 
@@ -120,5 +188,11 @@ object Cipher {
     }
 
   }
+
+  @scala.annotation.tailrec
+  def expandLastBlock(block: Array[Byte], acc: Int): (Array[Byte], Int) =
+    if (block.length < 8)
+      expandLastBlock(block.appended(0: Byte), acc + 1)
+    else block -> acc
 
 }
