a9a1bfb3d81212c091ff48457e61db006acbc48e
timofeygusev <gusev.t.s1@gmail.com>
2020-05-10 19:44:11 +0300

    minor update

--- modules/algorithms/src/main/scala/algos/cipher/Application.scala
+++ modules/algorithms/src/main/scala/algos/cipher/Application.scala
@@ -6,7 +6,7 @@ object Application extends App {
 
   def ecbR(): Unit = {
     val blowfish: BlowFishCipher = Cipher.blowFish("blowfish")
-    val input: Array[Byte] = ("b1l1o1w1f1i1s1h1" * 50000).getBytes()
+    val input: Array[Byte] = ("b1l1o1w1f1i1s1h1" * 500000).getBytes()
     val encrypted: Array[Byte] = blowfish.ecbEncrypt(input)
     val decrypted: Array[Byte] = blowfish.ecbDecrypt(encrypted)
     println(decrypted.sameElements(input))
@@ -14,7 +14,7 @@ object Application extends App {
 
   def cbcR(): Unit = {
     val blowfish: BlowFishCipher = Cipher.blowFish("blowfish")
-    val input: Array[Byte] = ("b1l1o1w1f1i1s1h1" * 50000).getBytes()
+    val input: Array[Byte] = ("b1l1o1w1f1i1s1h1" * 500000).getBytes()
     val iv = "11111111".getBytes
     val encrypted: Array[Byte] = blowfish.cbcEncrypt(input, iv)
     val decrypted: Array[Byte] = blowfish.cbcDecrypt(encrypted, iv)
@@ -23,7 +23,7 @@ object Application extends App {
 
   def pcbcR(): Unit = {
     val blowfish: BlowFishCipher = Cipher.blowFish("blowfish")
-    val input: Array[Byte] = ("b1l1o1w1f1i1s1h1" * 50000).getBytes()
+    val input: Array[Byte] = ("b1l1o1w1f1i1s1h1" * 500000).getBytes()
     val iv = "11111111".getBytes
     val encrypted: Array[Byte] = blowfish.pcbcEncrypt(input, iv)
     val decrypted: Array[Byte] = blowfish.pcbcDecrypt(encrypted, iv)
@@ -32,14 +32,14 @@ object Application extends App {
 
   def cfbR(): Unit = {
     val blowfish: BlowFishCipher = Cipher.blowFish("blowfish")
-    val input: Array[Byte] = ("b1l1o1w1f1i1s1h1" * 50000).getBytes()
+    val input: Array[Byte] = ("b1l1o1w1f1i1s1h1" * 500000).getBytes()
     val iv = "11111111".getBytes
     val encrypted: Array[Byte] = blowfish.cfbEncrypt(input, iv)
     val decrypted: Array[Byte] = blowfish.cfbDecrypt(encrypted, iv)
     println(decrypted.sameElements(input))
   }
-  //ecbR()
-  //cbcR()
-  //pcbcR()
+  ecbR()
+  cbcR()
+  pcbcR()
   cfbR()
 }
